fb644089071d23d50c88f58ecf889cdae753f192
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2025-04-20 12:26:26 -0300

    Create funcao.kt
    criando uma função em kotlin

---
+++ funcao.kt
@@ -0,0 +1,10 @@
+// Função que recebe um nome e imprime uma saudação
+fun sayHello(name: String) {
+    println("Olá, $name!")
+}
+
+// Função principal do programa (ponto de entrada)
+fun main() {
+    sayHello("Maria")      // Chamada da função
+    sayHello("João")       // Outra chamada da função
+}

066b3db085ffc5af7ee247252b978fde4e0b7753
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2025-04-20 12:40:22 -0300

    Create modificador-vararg.kt

---
+++ modificador-vararg.kt
@@ -0,0 +1,6 @@
+// O modificador vararg é usado quando você quer permitir que uma função receba um número variável de argumentos do mesmo tipo — ou seja, você não precisa saber de antemão quantos valores serão passados.
+/*
+Como funciona o vararg?
+Quando você usa vararg, a função aceita vários argumentos (ou nenhum) do tipo especificado, e internamente trata todos como um array.
+
+*/

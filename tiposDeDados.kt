afc0adc8a0a2a8230a82fcb853fb9e36632661fb
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2025-04-23 21:53:25 -0300

    Create tiposDeDados.kt
    Tipagem

---
+++ tiposDeDados.kt
@@ -0,0 +1,8 @@
+// Kotlin é uma linguagem de tipagem estática com inferência de tipos. Ou seja, você pode declarar o tipo se quiser, mas não é obrigatório:
+
+val cidade: String = "São Paulo"
+var altura: Double = 1.75
+
+// Ou deixar o Kotlin inferir:
+val cidade = "São Paulo"    // Kotlin infere que é String
+var altura = 1.75           // Kotlin infere que é Double

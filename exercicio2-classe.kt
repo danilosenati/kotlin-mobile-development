7027b22c7c8c00223db05f379f815d1259c4b149
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2025-05-07 11:15:46 -0300

    Create exercicio2-classe.kt
    segundo exercicio

---
+++ exercicio2-classe.kt
@@ -0,0 +1,23 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+
+/*
+Exercício prático com classes em Kotlin, agora focado em métodos que manipulam dados e encapsulamento.
+
+Criar uma classe que simule uma conta bancária simples.
+
+*/
+
+class ContaBancaria(val titular: String, var numeroConta: Int, var saldo: Double){
+
+}
+
+
+// FUNÇÃO PRINCIPAL
+
+fun main() {
+    
+        
+}

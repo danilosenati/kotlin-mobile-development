c9ff236ddc2c128ed4c0cd003324ba5ec174812c
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2025-05-07 10:59:04 -0300

    Update exercicio-classe.kt
    exercicio

--- exercicio-classe.kt
+++ exercicio-classe.kt
@@ -3,11 +3,16 @@
  * play.kotlinlang.org
  */
 
-class Produto(val nome, var preco, var quantidade){
+class Produto(val nome: String, var preco: Double, var quantidade: Int){
     
     init{
         if(preco < 0 || quantidade < 0){
         println("Preço e quantidade devem ser valores positivos.")
+        	if(preco < 0){
+                preco = 0.0
+            }else{
+                quantidade = 0
+            }
     	}
     }
     fun exibirInfo(){
@@ -15,13 +20,21 @@ class Produto(val nome, var preco, var quantidade){
         println("Preço: R$ $preco")
         println("Quantidade em estoque: $quantidade")
     }
-
-    
+   
 }
-
 
 // FUNÇÃO PRINCIPAL
+
+/*	Cria dois produtos, um com valores válidos e outro com valores inválidos.
+	Chama o método exibirInfo() para os dois. */
 fun main() {
     
+    val produto1 = Produto("Bicicleta",950.59, 1)
+    produto1.exibirInfo()
+    
+    println("***************************")
+    
+    var produto2 = Produto("Avião", -2.0, -2)
+    produto2.exibirInfo()
+    
 }
-
